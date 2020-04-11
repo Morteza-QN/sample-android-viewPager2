45663f11e1e3e7d691f6ce25ab8ea8a146379fe9
morteza <mortezaghorbani3@gmail.com>
2020-04-11 12:14:46 +0430

    added TabLayout in viewPager2

--- app/src/main/java/com/example/sampleandroidviewpager2/MainActivity.java
+++ app/src/main/java/com/example/sampleandroidviewpager2/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.sampleandroidviewpager2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,31 @@ public class MainActivity extends AppCompatActivity {
         SlideAdapter slideAdapter = new SlideAdapter(this);
         viewPager2.setAdapter(slideAdapter);
         viewPager2.setOrientation(ViewPager2.ORIENTATION_VERTICAL);
+        viewPager2.setCurrentItem(2);
+
+        TabLayout tabLayout = findViewById(R.id.tb_main);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+                    case 0:
+                        tab.setText("A");
+                        break;
+                    case 1:
+                        tab.setText("B");
+                        break;
+                    case 2:
+                        tab.setText("C");
+                        break;
+                    case 3:
+                        tab.setText("D");
+                        break;
+                    case 4:
+                        tab.setText("E");
+                        break;
+                }
+            }
+        });
+        tabLayoutMediator.attach();
     }
 }
