01ea44602ab64b1532e1fbcf4ee2d90cba2ea141
morteza <mortezaghorbani3@gmail.com>
2020-04-11 11:39:07 +0430

    create sample ViewPager2

--- app/src/main/java/com/example/sampleandroidviewpager2/MainActivity.java
+++ app/src/main/java/com/example/sampleandroidviewpager2/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.sampleandroidviewpager2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager2   viewPager2   = findViewById(R.id.vp2_main);
+        SlideAdapter slideAdapter = new SlideAdapter(this);
+        viewPager2.setAdapter(slideAdapter);
     }
 }
--- app/src/main/java/com/example/sampleandroidviewpager2/SlideFragment.java
+++ app/src/main/java/com/example/sampleandroidviewpager2/SlideFragment.java
@@ -1,5 +1,36 @@
 package com.example.sampleandroidviewpager2;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SlideFragment extends Fragment {}
+public class SlideFragment extends Fragment {
+    private static final String EXTRA_KEY_DATA = "data";
+
+    public static SlideFragment newInstance(String data) {
+        Bundle        args     = new Bundle();
+        SlideFragment fragment = new SlideFragment();
+        args.putString(EXTRA_KEY_DATA, data);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_slide, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView slideTv = view.findViewById(R.id.tv_fragment_slide);
+        slideTv.setText(getArguments() != null ? getArguments().getString(EXTRA_KEY_DATA) : "null");
+    }
+}
